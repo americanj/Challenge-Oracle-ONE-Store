24f1b0a063d707a0aaf0fa2fad7ca06c0f7f606e
oliveiradeigor <lrmcpe22@gmail.com>
2022-09-08 20:42:56 -0300

    adicionando funcionalidade em aparecer o botão de deletar e editar estando como administrador

--- assets/js/validacao/autenticacao-do-adm.js
+++ assets/js/validacao/autenticacao-do-adm.js
@@ -1,19 +1,41 @@
 
 
+// Pega dados do Session
 var pegaDadosLogin = localStorage.getItem('dadosLogin');
-var pegaAutenticacao = sessionStorage.getItem('autenticacao');
-
-const btnAddProduto = document.querySelector('[data-btnAddProduto]');
-//console.log(pegaAutenticacao);
-
 
 const loginLocal = JSON.parse(pegaDadosLogin).login;
 const senhaLocal = JSON.parse(pegaDadosLogin).senha;
 
+
+// Pega autenticação
+var pegaAutenticacao = sessionStorage.getItem('autenticacao');
+
+
+// funções do administrador
+const btnAddProduto = document.querySelector('[data-btnAddProduto]');
+
+const lapis = document.querySelectorAll('.games__flex-lapis');
+const lixeira = document.querySelectorAll('.games__flex-trash');
+
+
+
+
+//Verifica se o token coresponde, e dá os privilégios de administrador
 if(pegaAutenticacao == 'permitido'){
      btnAddProduto.classList.remove('games__botao-hidden');
-    
+
+     lapis.forEach(function(elemento){
+          elemento.classList.remove('games__flex-lapis-hidden')
+     })
+
+     lixeira.forEach(function(elemento){
+          elemento.classList.remove('games__flex-trash-hidden')
+     })
+ 
 }
+
+
+
 
 
 
