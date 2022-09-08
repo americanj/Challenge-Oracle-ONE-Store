29cc74379fedb21baf5ea989c0e69f217bf8d577
oliveiradeigor <lrmcpe22@gmail.com>
2022-09-08 14:10:11 -0300

    deletendo o trecho do add event listener no input enviar form

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -4,10 +4,10 @@
 
 
 const inputNome = document.querySelector('[data-inputNome]');
-inputNome.addEventListener('blur', ()=>{
-    if(inputNome.value == ''){
+inputNome.addEventListener('blur', () => {
+    if (inputNome.value == '') {
         inputNome.classList.add('rodape__input-error');
-        
+
     } else {
         inputNome.classList.remove('rodape__input-error');
     }
@@ -15,22 +15,11 @@ inputNome.addEventListener('blur', ()=>{
 
 
 const inputTextArea = document.querySelector('[data-inputMensagem]');
-inputTextArea.addEventListener('blur', ()=>{
-    if(inputTextArea.value == ''){
+inputTextArea.addEventListener('blur', () => {
+    if (inputTextArea.value == '') {
         inputTextArea.classList.add('rodape__textarea-error')
-        
+
     } else {
         inputTextArea.classList.remove('rodape__textarea-error');
     }
 })
-
-
-const formulario = document.querySelector('[data-formulario]');
-formulario.addEventListener('click', ()=>{
-    location.reload();
-})
-
-/*
-input.addEventListener('blur', function (evento) {
-    valida(evento.target)
-})*/
