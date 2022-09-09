234f9f7f31abd38a2cf4cfcc76a3e233899f4b4e
oliveiradeigor <lrmcpe22@gmail.com>
2022-09-09 18:29:40 -0300

    fazendo aparecer os itens na tela de todos os produtos

--- assets/js/service/produto-service.js
+++ assets/js/service/produto-service.js
@@ -1,4 +1,8 @@
 
+
+
+
+/*
 const criaNovoJogo = function(imagem, nome, preco){
     const divNovoJogo = document.createElement('div')
     divNovoJogo.classList.add('games__flex-card');
@@ -15,8 +19,9 @@ const criaNovoJogo = function(imagem, nome, preco){
         divNovoJogo.innerHTML = conteudo;
         return divNovoJogo;
 }
+*/
 
-
+/*
 var pegarJogos = localStorage.getItem('produtos');
 
 const imagem = JSON.parse(pegarJogos)[0].imagem
@@ -31,7 +36,7 @@ const container = document.querySelector('[data-promocao]');
 container.appendChild(criaNovoJogo(imagem, nome, preco))
 
 
-/*
+
 const http = new XMLHttpRequest()
 http.open('GET', 'http://localhost:300/profile')
 
@@ -39,7 +44,39 @@ http.onload = function(){
     const data = http.response
     console.log(data);
 }
+
 */
+const containerProdutos = document.querySelector('[data-todosProdudos]');
+
+
+function criaNovoJogo(imagem, nome, preco){
+  
+    const divNovoJogo = document.createElement('div')
+    divNovoJogo.classList.add('games__flex-card');
+
+    const conteudo = 
+    `
+        <img data-BtnLapis="" class="games__flex-lapis games__flex-lapis-hidden"src="assets/img/lapis.svg" alt="">
+        <img data-BtnTrash="" class="games__flex-trash games__flex-trash-hidden" src="assets/img/trash.svg" alt="">
+        <img class="games__imagem-jogo" src="${imagem}" alt="">
+        <span class="games__nome-produto">${nome}</span>
+        <span class="games__preco-produto">R$ ${preco}</span>
+        <a href="produto.html" class="games__ver-produto">Ver produto</a>
+    `
+    divNovoJogo.innerHTML = conteudo
+    return divNovoJogo;
+    
+}
+
+
+const pegarJogos = localStorage.getItem('produtos');
+const jogosConvertidosObjeto = JSON.parse(pegarJogos)
+
+jogosConvertidosObjeto.forEach(elemento => {
+    containerProdutos.appendChild(criaNovoJogo(elemento.imagem, elemento.nome, elemento.preco))
+})
+
+
 
 
 
